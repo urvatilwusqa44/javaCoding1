1584d7aace7cb90ce416d41dcb69dd955736472d
urvatilwusqa44 <142150448+urvatilwusqa44@users.noreply.github.com>
2023-09-13 03:22:35 +1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+//output questions
+//Question No.1
+let output="Hello World!";
+console.log(output);
+//Question No.2
+let identity="My name is Urva til Wusqa."
+let learn="I am learning Javascript via Full Stack Bootcamp of DiaaTech."
+let work="I will become the best developer by hard work."
+let code="I am making commitment to do code with consistency."
+console.log(identity);
+console.log(learn);
+console.log(work);
+console.log(code);
+//Variables
+//Question No.1
+let name="Urva til wusqa";
+var age=21;
+console.log("Name:"+name);
+console.log("Age:"+age+"years");
+//Question no.2
+let firstName="Urva";
+let secondName="til wusqa";
+let language="JavaScript";
+let introduction=`My name is ${firstName} ${secondName}.I am learning ${language} via Full Stack Development of DiaTech.
+I will become the best developer by hard work. I am making commitment to do code with consistency.`
+console.log(introduction);
+//Question no.3
+/*let num1=prompt("please enter first number");
+let num2=prompt("Please enter second number");
+var sum=Number(num1)+Number(num2);
+var minus=Number(num1)-Number(num2);
+var multiply=Number(num1)*Number(num2);
+var divide=Number(num1)/Number(num2);
+console.log("Sum="+sum);
+console.log("Minus="+minus);
+console.log("multiplication="+multiply);
+console.log("Division="+divide);*/
+
+//BMI
+//Question no.1
+var MarkHeight=20;
+var MarkMass=40;
+console.log("Mark Height="+MarkHeight+"meters");
+console.log("Mark Mass="+MarkMass+"kg");
+var JohnHeight=30;
+var JohnMass=50;
+console.log("John Height="+JohnHeight+"m");
+console.log("John Mass="+JohnMass+"kg");
+//Question no.2
+var height1=20;
+var mass1=40
+let MarkBMI=Number(mass1)/(Number(height1)*Number(height1))*(Number(mass1)*Number(height1));
+console.log("BMI of Marks="+MarkBMI+"kgm");
+var height2=30;
+var mass2=50
+let JohnBMI=Number(mass2)/(Number(height2)*Number(height2))*(Number(mass2)*Number(height2));
+console.log("BMI of John="+JohnBMI+"kgm");
+//Question no.3
+let markhigherBMI=true;
+var BMIofMark=80;
+ var BMIofJohn=83;
+ if(BMIofJohn>BMIofMark)
+ {
+    console.log("John BMI is higher.");
+ }
+ else
+ {
+    console.log("Mark BMI is higher.");
+ }
+
